be957b82246c73d41333c6dcdcbbac709b3a875f
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-07-02 13:43:02 +0530

    Get CQRS

---
+++ Client-Project/Client.Application/Features/Product/Dtos/GetByIdCompanyDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Application.Features.Product.Dtos
+{
+    public class GetByIdCompanyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Addresss { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int IsActive { get; set; }
+    }
+}
