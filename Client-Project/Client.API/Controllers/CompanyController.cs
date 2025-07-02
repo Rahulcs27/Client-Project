622b70ef6a2c1f8d0554e199fc2cb41a27f075c1
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-07-02 16:06:01 +0530

    Before Mapping

---
+++ Client-Project/Client.API/Controllers/CompanyController.cs
@@ -0,0 +1,30 @@
+﻿using Client.Application.Features.Product.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CompanyController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCompanies()
+        {
+            var result = await _mediator.Send(new GetAllCompanyQuery());
+            return Ok(result);
+        }
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetCompanyById(int Id)
+        {
+            var result = await _mediator.Send(new GetByIdCompanyQuery(Id));
+            return Ok(result);
+        }
+    }
+}
--- Client-Project/Client.Persistence/Repositories/CompanyRepository.cs
+++ Client-Project/Client.Persistence/Repositories/CompanyRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Client.Application.Interfaces;
 
 namespace Client.Persistence.Repositories
 {
-    internal class CompanyRepository
+    public class CompanyRepository : ICompanyRepository
     {
     }
 }
